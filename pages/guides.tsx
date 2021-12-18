ff29fab1588f0f47b5475a847d541c3a3ba1a246
adham618 <adhamtarek291@gmail.com>
2021-12-18 20:10:13 +0200

    fix the error showe everytime

--- pages/guides.tsx
+++ pages/guides.tsx
@@ -1,12 +1,16 @@
-import { useEffect } from 'react'
+import { useContext, useEffect } from 'react'
+import { AuthContext } from '../stores/AuthContext'
 import styles from '../styles/Guides.module.css'
 
 const Guides: React.FC = () => {
+  const { user } = useContext(AuthContext)
+
   useEffect(() => {
     fetch("/.netlify/functions/guides")
       .then(res => res.json())
       .then(data => console.log(data))
-  }, [])
+  }, [user])
+
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
